76c8e232ca56255786b1f93d86b4ef3457f2f8e4
Badr <badr6@BadrAlmousa>
2024-12-03 18:40:17 +0300

    homework

---
+++ bin/day3_ass.dart
@@ -0,0 +1,28 @@
+void main() {
+  // print(swap(1));
+  // print(swap(8));
+
+  var result = checkLetters("aaaAAAAaaaabcdefghijklmNOPQRSTUVWXYZ");
+
+  print(result);
+}
+
+int swap(int number) => 1 - number;
+
+bool checkLetters(String str) {
+  String toLowercase = str.toLowerCase();
+
+  List list = toLowercase.split("");
+  Set set = list.toSet();
+
+  set.removeAll({" "});
+  print(set);
+
+  print(set.length);
+  return set.length == 26;
+
+
+  
+}
+
+  
---
+++ test/day3_ass_test.dart
@@ -0,0 +1,8 @@
+import 'package:day3_ass/day3_ass.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
